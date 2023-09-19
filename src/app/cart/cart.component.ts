a12dba781388a3e2ef5f46361535ca4ed891e677
FixTheError <89999029+FixTheError@users.noreply.github.com>
2023-09-19 11:39:37 -0500

    2 files modified

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms'
 import { CartService } from '../cart.service';
 
 @Component({
@@ -7,8 +8,23 @@ import { CartService } from '../cart.service';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent {
+
   items = this.cartService.getItems();
+
+  checkoutForm = this.FormBuilder.group({
+    name: '',
+    address: '',
+  })
+
   constructor(
-    private cartService: CartService
+    private cartService: CartService,
+    private FormBuilder: FormBuilder,
   ) { }
+
+  OnSubmit(): void{
+    //Process checkout data.
+    this.items = this.cartService.clearCart();
+    console.warn('Your order has been submitted', this.checkoutForm.value);
+    this.checkoutForm.reset();
+  }
 }
